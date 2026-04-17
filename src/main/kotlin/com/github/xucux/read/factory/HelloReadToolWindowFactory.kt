6d1216cba47e263b133982b85704a2c48f1fa784
xucux <xucux@foxmail.com>
2026-04-17 16:44:01 +0800

    refactor(ui): 移除设置窗口功能

--- src/main/kotlin/com/github/xucux/read/factory/HelloReadToolWindowFactory.kt
+++ src/main/kotlin/com/github/xucux/read/factory/HelloReadToolWindowFactory.kt
@@ -40,13 +40,13 @@ class HelloReadToolWindowFactory : ToolWindowFactory {
         val bookshelfToolWindow = BookshelfToolWindow(project)
         val bookReaderToolWindow = BookReaderToolWindow(project)
         val chapterListWindow = ChapterListToolWindow(project)
-        val settingWindows = SettingsToolWindow(project)
+//        val settingWindows = SettingsToolWindow(project)
         
         // 创建内容
         val bookshelfContent = ContentFactory.getInstance().createContent(bookshelfToolWindow, TabConstants.BOOKSHELF_TAB, false)
         val bookReaderContent = ContentFactory.getInstance().createContent(bookReaderToolWindow, TabConstants.READER_TAB, false)
         val chapterListContent = ContentFactory.getInstance().createContent(chapterListWindow, TabConstants.CHAPTER_LIST_TAB, false)
-        val settingsContent = ContentFactory.getInstance().createContent(settingWindows, TabConstants.SETTINGS_TAB, false)
+//        val settingsContent = ContentFactory.getInstance().createContent(settingWindows, TabConstants.SETTINGS_TAB, false)
         
         // 添加ContentManagerListener来监听内容选择事件
         toolWindow.contentManager.addContentManagerListener(object : ContentManagerListener {
@@ -62,7 +62,7 @@ class HelloReadToolWindowFactory : ToolWindowFactory {
         toolWindow.contentManager.addContent(bookshelfContent)
         toolWindow.contentManager.addContent(bookReaderContent)
         toolWindow.contentManager.addContent(chapterListContent)
-        toolWindow.contentManager.addContent(settingsContent)
+//        toolWindow.contentManager.addContent(settingsContent)
     }
     
     override fun shouldBeAvailable(project: Project): Boolean = true
